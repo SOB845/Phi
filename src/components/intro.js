fe3ab94c8dd73a2451a3643bb7bd161ed78f509f
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2019-07-31 18:48:25 +0430

    introductory text

---
+++ src/components/intro.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Intro extends Component{
+    render(){
+        return[
+            <h1 className='intr1'>Φ.ai</h1>,
+            <h2 className='intr2'>An Image Processing Platform</h2>,
+        ];
+    }
+}
+export default Intro;
