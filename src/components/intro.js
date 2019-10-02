ff5941ed1dfdbae1007eeea408c79c2e0ef28abe
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2019-10-02 22:33:29 +0330

    Update intro.js

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,7 +4,7 @@ class Intro extends Component{
     render(){
         return[
             <h1 className='intr1'>Φ.ai</h1>,
-            <h2 className='intr2'>An Image Processing Platform</h2>,
+            <h2 className='intr2'>A Computer Vision Research Platform</h2>,
         ];
     }
 }
