ee14c797774b1017f80f1051b8c039a06dd1a09c
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2019-10-04 14:35:21 +0330

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -14,22 +14,18 @@ class Nav extends Component{
                         <Nav.Link href="/home">Home</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="link-1">Sign in</Nav.Link>
+                        <Nav.Link eventKey="signin">Sign in</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="link-2">All papers</Nav.Link>
+                        <Nav.Link eventKey="papers">All papers</Nav.Link>
                     </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link eventKey="disabled" disabled>
-                        Disabled
-                        </Nav.Link>
-                    </Nav.Item>
+
                     <NavDropdown title="Dropdown" id="nav-dropdown">
-                        <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="4.1">About Us</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="4.2">Contact Us</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="4.3">Contribute</NavDropdown.Item>
                         <NavDropdown.Divider />
-                        <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
+                        <NavDropdown.Item eventKey="4.4">Visit Pi.ai</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
             </div>
