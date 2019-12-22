5eeb53f38fad1f0b6abb88063104b78236db2b38
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2019-12-22 17:58:28 +0330

    Update signup.js

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,34 +1,41 @@
 import React, { Component } from 'react';
 
-
 class Signup extends Component {
 
-    state={
-        isVisible: true
+    constructor(props){
+        super(props);
+        this.isVisible = {isVisible:true};
     }
 
     onClickEvent = () =>{
         this.setState({
-            isVisible: !this.state.isVisible
+            isVisible : false
         })
     }
-
 
     render(){
         return(
             <div className = "col-md-8 mb-4">
-                    <div className= "card" style={isVisible ? {color:"black", textAlign:"center"}: null}>
+                    <div className= "card" style={{color:"black", textAlign:"center"}}>
                             <div className= "card-header d-flex justify-content-between">
                                 <h4 className="d-inline">Sign up</h4>
+                                <br/>
                                     <div className="card-body">
                                         <form>
                                             <label>
                                                     Name:
-                                                    <input type="text" name="name" placeholder="Enter your real name"></input>
-
+                                                    <input type="text" name="name" placeholder="        Enter your full name"></input>
+                                                    <br/>
+                                                    <br/>
                                                     Email:
-                                                    <input type= "text" name="email address" placeholder="Enter your email"></input>
-                                                    <button type="submit", value="Create account" onClick={this.onClickEvent.bind(this)}>Sign in</button>
+                                                    <input type= "text" name="email address" placeholder= "         Enter your email"></input>
+                                                    <br/>
+                                                    <br/>
+                                                    University: 
+                                                    <input type="text" name="University" placeholder="      Where do you study?"></input>
+                                                    <br/>
+                                                    <br/>
+                                                    <button className="sign_in" type="submit" value="create account">Sign in</button>
                                             </label>
                                         </form>
                                     </div>
